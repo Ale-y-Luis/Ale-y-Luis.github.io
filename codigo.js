8757021662df5d8ffe626fe730fe24b4515bb440
Ale-y-Luis <ldlerrgo@gmail.com>
2024-12-03 21:08:30 -0500

    Menu movil
    Y ajustes de layout

--- codigo.js
+++ codigo.js
@@ -1,25 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const inputCodigo = document.getElementById("codigo_secreto");
-
-
-    inputCodigo.addEventListener("keypress", (event) => {
-        if (event.key === "Enter") {
-            event.preventDefault();
-            validarCodigo(inputCodigo.value);
+    if (sessionStorage.getItem("sesionValida") !== "true") {
+        if (!window.location.pathname.endsWith("index.html")) {
+            window.location.href = "index.html";
         }
-    });
+    }
+
+    const inputCodigo = document.getElementById("codigo_secreto");
+    if (inputCodigo) {
+        inputCodigo.addEventListener("keypress", (event) => {
+            if (event.key === "Enter") {
+                event.preventDefault();
+                validarCodigo(inputCodigo.value);
+            }
+        });
+    }
 });
 
 /**
-@param {string} codigo
+ * @param {string} codigo
  */
-
 function validarCodigo(codigo) {
     const codigoCorrecto = "12345";
 
     if (codigo === codigoCorrecto) {
+        sessionStorage.setItem("sesionValida", "true");
         alert("Bienvenida amor mío ❤️");
-        window.location.href = "./inicio.html";
+        window.location.href = "inicio.html";
     } else {
         alert("¿Quién eres? ¿Un espía? ¿Para quién? 👊");
     }
